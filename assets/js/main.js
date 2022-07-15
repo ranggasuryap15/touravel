625c6f4fbfc33e4a3d1a1454942a06098230249e
ranggasuryap15 <ranggasurya.313@gmail.com>
2022-07-16 01:52:53 +0700

    membuat product slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,21 @@ menuOpen.addEventListener('click', function() {
     menuOpen.classList.toggle('bx-x');
     menuWrapper.classList.toggle('active');
 })
+
+var swiper = new Swiper(".mySwiper", {
+    grabCursor: true,
+    centeredSlides: false,
+    spaceBetween: 30,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 0,
+        modifier: 1,
+        slideShadows: true,
+    },
+    navigation: {
+        nextEl: ".bxs-chevron-right-circle",
+        prevEl: ".bxs-chevron-left-circle",
+    },
+});
